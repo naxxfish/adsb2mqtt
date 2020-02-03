711881a176d77ba2407831b595a2b9d8716fbe96
Chris Roberts <chris@naxxfish.net>
2020-02-03 21:42:09 +0000

    set error correction on

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const AircraftStore = require('mode-s-aircraft-store')
 const Decoder = require('mode-s-decoder')
 
 const BeastClient = require('./lib/beastclient')
-const decoder = new Decoder()
+const decoder = new Decoder({
+  fixErrors: true,
+  aggressive: true
+})
 
 nconf.env('__').file('./config/defaults.json').required([
   'modes:host',
@@ -44,23 +47,23 @@ setInterval(function () {
   log.info({ event: 'stats', totalAircraft: allAircraft.length })
   allAircraft.forEach(function (aircraft) {
     if (aircraft.callsign) {
-      if (aircraft.altitude !== null && aircraft.altitude !== undefined) {
+      if (aircraft.altitude !== 0) {
         log.debug({ event: 'altitude', callsign: aircraft.callsign, altitude: aircraft.altitude })
         mqttClient.publish(`${recieverId}/aircraft/${aircraft.callsign}/altitude`, JSON.stringify(aircraft.altitude), mqttPublishOptions)
       }
-      if (aircraft.lat !== null || aircraft.lng !== null) {
+      if (aircraft.lat !== 0 || aircraft.lng !== 0) {
         log.debug({ event: 'position', callsign: aircraft.callsign, lat: aircraft.lat, lon: aircraft.lng })
         mqttClient.publish(`${recieverId}/aircraft/${aircraft.callsign}/position`, JSON.stringify({ lat: aircraft.lat, long: aircraft.lng }), mqttPublishOptions)
       }
-      if (aircraft.speed !== null) {
+      if (aircraft.speed !== 0) {
         log.debug({ event: 'ground_speed', callsign: aircraft.callsign, speed: aircraft.speed })
         mqttClient.publish(`${recieverId}/aircraft/${aircraft.callsign}/speed`, JSON.stringify(aircraft.speed), mqttPublishOptions)
       }
-      if (aircraft.heading !== null) {
+      if (aircraft.heading !== 0) {
         log.debug({ event: 'heading', callsign: aircraft.callsign, heading: aircraft.heading })
         mqttClient.publish(`${recieverId}/aircraft/${aircraft.callsign}/heading`, JSON.stringify(aircraft.heading), mqttPublishOptions)
       }
-      if (aircraft.count !== null) {
+      if (aircraft.count !== 0) {
         log.debug({ event: 'message_count', callsign: aircraft.callsign, count: aircraft.count })
         mqttClient.publish(`${recieverId}/aircraft/${aircraft.callsign}/msg_count`, JSON.stringify(aircraft.count), mqttPublishOptions)
       }
